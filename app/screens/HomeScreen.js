26161440a8e0f49146c1508d24c7b2f58159ba71
charles <nccharles1@gmail.com>
2019-05-20 14:06:02 +0200

    removed unused functions

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
   ScrollView,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-import { Icon,WebBrowser } from 'expo';
+import { Icon} from 'expo';
 import styles from './styles/style'
 import { MonoText } from '../components/StyledText';
 import Colors from '../constants/Colors';
@@ -36,37 +34,4 @@ export default class HomeScreen extends React.Component {
       </ScrollView>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
